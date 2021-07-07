f2be87aa2c10bd3ec1695f60cab8171d1eda3461
wangxu <wangxu@miao.cn>
2021-07-07 11:19:28 +0800

    update

--- TypeScript/flow/src/flow.js
+++ TypeScript/flow/src/flow.js
@@ -1,5 +1,13 @@
 //@flow
+//参数的类型注解
 function sum(a:number,b:number) {return a + b};
 sum(100,200)
 sum(100,'200')
 
+//变量的类型注解，不可赋其他类型的值
+let num: number = 100;
+
+//返回的类型注解
+function foo():number {
+    return 100
+}
